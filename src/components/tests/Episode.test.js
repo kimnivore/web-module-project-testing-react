fe0987f8d826ea214506e3b0d9388974c8bd4c3b
Kim Nguyen <kimnivore@gmail.com>
2022-01-24 12:23:23 -0800

    episode component testing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -5,9 +5,19 @@ import Episode from './../Episode';
 
 
 test("renders without error", () => {
-    render(<Episode />);
+    render(<Episode episode={{}} />);
 });
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", () =>{
+    render(<Episode episode={{summary: 'summary test'}} />);
+    let summary = screen.queryByText(/summary test/i);
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent('summary test');
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders default image when image is not defined", () =>{
+    render(<Episode episode={{summary: 'summary test', image: null}} />);
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+    expect(image).toBeInTheDocument();
+});
